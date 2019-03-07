910db737a23f4e215495401f90c1e1c9f2aaee36
alexanderinf <rinfgit10>
2019-03-07 10:44:23 +0200

    master

---
+++ Scripts/JavaScriptDial.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+	$('.num').click(function () {
+		alert("er")
+		var num = $(this);
+		var text = $.trim(num.find('.txt').clone().children().remove().end().text());
+		var telNumber = $('#telNumber');
+		$(telNumber).val(telNumber.val() + text);
+	});
+
+});
